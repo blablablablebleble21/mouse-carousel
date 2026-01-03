cf600a60fb4af598a1d28435fc1a10ca1819342a
Ziv Glam <glamziv9@gmail.com>
2026-01-03 17:10:34 +0200

    removed duplicate ling.

--- run-app.bat
+++ run-app.bat
@@ -2,8 +2,5 @@
 :: Navigate to the folder where this script is located
 cd /d "%~dp0"
 
-:: Rebuild native modules for Electron
-.\node_modules\.bin\electron-rebuild -f -w robotjs
-
 :: Launch the Electron app
 npm start
